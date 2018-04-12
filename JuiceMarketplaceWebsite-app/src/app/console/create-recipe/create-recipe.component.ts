b976e09c71491850f3fdbfe11f9b975dbaf26cc6
liefersfl <florian@liefers.com>
2018-04-12 15:46:39 +0200

    upgraded tdm-common and cocktailconfigurator updated components route

--- JuiceMarketplaceWebsite-app/src/app/console/create-recipe/create-recipe.component.ts
+++ JuiceMarketplaceWebsite-app/src/app/console/create-recipe/create-recipe.component.ts
@@ -2,6 +2,7 @@ import {Injectable, ViewChild, ElementRef, HostListener} from '@angular/core';
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Router, ActivatedRoute} from '@angular/router';
+import { MatDialog } from '@angular/material';
 import * as $ from 'jquery';
 
 import {MarketplaceService} from '../services/marketplace.service';
@@ -15,7 +16,7 @@ import {Cocktail} from 'tdm-common'
 import {CocktailComponent} from 'tdm-common'
 import {CocktailLayer} from 'tdm-common'
 import {ComponentService} from 'tdm-common'
-import {ComponentListComponent, DragAndDropService, BeakerComponent} from 'cocktail-configurator'
+import {ComponentListComponent, DragAndDropService, BeakerComponent, ComponentListDialogComponent} from 'cocktail-configurator'
 
 @Component({
     selector: 'app-create-recipe',
@@ -33,6 +34,10 @@ export class CreateRecipeComponent implements OnInit {
     cocktail: Cocktail;
     components: CocktailComponent[] = [];
 
+    showRecommendedComponents = false
+    showInstalledComponents = false
+    showAvailableComponents = true
+
     // components: TdmComponent[];
     licenseFees: number[] = [0.25, 0.5, 0.75, 1.00];
     spinnerCounter = 0;
@@ -44,6 +49,7 @@ export class CreateRecipeComponent implements OnInit {
     recipeCount = 0;
 
     constructor(private marketplaceService: MarketplaceService,
+                private dialog: MatDialog,
                 private recipeService: RecipeService,
                 private http: HttpClient,
                 private router: Router,
@@ -53,27 +59,7 @@ export class CreateRecipeComponent implements OnInit {
 
         this.cocktail = new Cocktail();
         this.cocktail.amount = 100;
-        componentService.setRecommendComponentIds(
-            [
-                "570a5df0-a044-4e22-b6e6-b10af872d75c", // Testing: Mineralwasser
-                "198f1571-4846-4467-967a-00427ab0208d", // Testing: Apfelsaft
-                "f6d361a9-5a6f-42ad-bff7-0913750809e4", // Testing: Orangensaft
-                "fac1ee6f-185f-47fb-8c56-af57cd428aa8", // Testing: Mangosaft
-                "0425393d-5b84-4815-8eda-1c27d35766cf", // Testing: Kirschsaft
-                "4cfa2890-6abd-4e21-a7ab-17613ed9a5c9", // Testing: Bananensaft
-                "14b72ce5-fec1-48ec-83ff-24b124f98dc8", // Testing: Maracuijasaft
-                "bf2cfd66-5b6f-4655-8e7f-04090308f6db", // Testing: Ananassaft
-                "d5a0e575-5767-4496-bac5-0d661ed5b5ee", // Production: Mineralwasser
-                "f391d4bf-5b88-4583-9be7-15f029f525f5", // Production: Apfelsaft
-                "db46f436-2068-4650-af65-46e504aaa238", // Production: Orangensaft
-                "1f6a7ae1-f6f7-4613-adda-4cfdba28b167", // Production: Mangosaft
-                "cfc32d53-a386-4023-ba43-bc6996e11b72", // Production: Kirschsaft
-                "18851cce-2526-4365-8381-c39c33f95542", // Production: Bananensaft
-                "eade7f3b-ec8c-4658-9a84-a98ddca4a352", // Production: Maracuijasaft
-                "6c3e5356-b8e0-4978-9f68-c5f7f59ce2f4", // Production: Ananassaft
-            ]
-        );
-        componentService.components.subscribe(components => {
+        componentService.availableComponents.subscribe(components => {
             this.components = components;
         })
     }
@@ -173,7 +159,24 @@ export class CreateRecipeComponent implements OnInit {
                 }
             }
         });
+    }
 
+    selectComponent(callback: (component: CocktailComponent) => any) {
+        let dialogRef = this.dialog.open(ComponentListDialogComponent, {
+            width: '300px',
+            data: {
+                showRecommended: this.showRecommendedComponents,
+                showInstalled: this.showInstalledComponents,
+                showAvailable: this.showAvailableComponents,
+            },
+            autoFocus: false
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                callback(result)
+            }
+        });
     }
 
 }
