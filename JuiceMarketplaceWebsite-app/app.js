bd3e6366020f58a3613d436d5bec1127b4c86b7f
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-05-22 14:43:14 +0200

    Fixed some issues in status code handling in case of nested errors

--- JuiceMarketplaceWebsite-app/app.js
+++ JuiceMarketplaceWebsite-app/app.js
@@ -73,7 +73,7 @@ app.all('*', function (req, res, next) {
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    let err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
@@ -112,7 +112,7 @@ if (app.get('env') === 'development') {
         }
 
         console.error(err.stack);
-        res.status(err.statusCode || 500);
+        res.status(err.status || 500);
         res.json({
             message: err.message,
             error: err
@@ -122,8 +122,8 @@ if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         console.error(err);
         // Send error details to the client only when the status is 4XX
-        if (err.statusCode && err.statusCode >= 400 && err.statusCode < 500) {
-            res.sendStatus(err.statusCode);
+        if (err.status && err.status >= 400 && err.status < 500) {
+            res.sendStatus(err.status);
         }
         else {
             res.status(500);
