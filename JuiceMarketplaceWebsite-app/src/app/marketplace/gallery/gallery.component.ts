677ee1457f33f726f8c8500d3056983a586714fe
Christian Görg <christian.goerg@de.trumpf.com>
2018-08-29 10:30:12 +0200

    added technology icon

--- JuiceMarketplaceWebsite-app/src/app/marketplace/gallery/gallery.component.ts
+++ JuiceMarketplaceWebsite-app/src/app/marketplace/gallery/gallery.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {TechnologydataService} from "../services/technologydata.service";
 import {TdmTechnologyData} from "tdm-common";
 
+
+
+
 @Component({
     selector: 'app-gallery',
     templateUrl: './gallery.component.html',
@@ -11,6 +14,8 @@ import {TdmTechnologyData} from "tdm-common";
 export class GalleryComponent implements OnInit {
 
     technologyData: TdmTechnologyData[];
+    ULTIMAKER_TECHNOLOGY_UUID = "adb4c297-45bd-437e-ac90-2aed14f6b882";
+    COCKTAIL_TECHNOLOGY_UUID = "da17a8fc-a5b3-40a4-b6a5-276667db027a";
 
     constructor(private technologDataService: TechnologydataService) {
     }
